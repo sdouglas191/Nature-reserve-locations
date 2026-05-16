2055dff9c7994634519b95e21cbdcb8a8644780c
sdouglas191 <115540757+sdouglas191@users.noreply.github.com>
2026-05-16 19:40:57 +0100

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -1,38 +1,52 @@
 var wms_layers = [];
 
 
-        var lyr_BingVirtualEarth_0 = new ol.layer.Tile({
-            'title': 'Bing VirtualEarth',
-            'type':'base',
+        var lyr_OSMStandard_0 = new ol.layer.Tile({
+            'title': 'OSM Standard',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: '<a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors, CC-BY-SA</a>',
+                url: 'http://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+
+        var lyr_GoogleSatelliteHybrid_1 = new ol.layer.Tile({
+            'title': 'Google Satellite Hybrid',
             'opacity': 1.000000,
             
             
             source: new ol.source.XYZ({
             attributions: ' ',
-                url: 'http://ecn.t3.tiles.virtualearth.net/tiles/a{q}.jpeg?g=1'
+                url: 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}'
             })
         });
-var format_OSM_railways_1 = new ol.format.GeoJSON();
-var features_OSM_railways_1 = format_OSM_railways_1.readFeatures(json_OSM_railways_1, 
+var format_NaturesReserves_2 = new ol.format.GeoJSON();
+var features_NaturesReserves_2 = format_NaturesReserves_2.readFeatures(json_NaturesReserves_2, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_OSM_railways_1 = new ol.source.Vector({
+var jsonSource_NaturesReserves_2 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_OSM_railways_1.addFeatures(features_OSM_railways_1);
-var lyr_OSM_railways_1 = new ol.layer.Vector({
+jsonSource_NaturesReserves_2.addFeatures(features_NaturesReserves_2);
+var lyr_NaturesReserves_2 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_OSM_railways_1, 
-                style: style_OSM_railways_1,
-                popuplayertitle: 'OSM_railways',
+                source:jsonSource_NaturesReserves_2, 
+                style: style_NaturesReserves_2,
+                popuplayertitle: 'Natures Reserves',
                 interactive: true,
-                title: '<img src="styles/legend/OSM_railways_1.png" /> OSM_railways'
-            });
+    title: 'Natures Reserves<br />\
+    <img src="styles/legend/NaturesReserves_2_0.png" /> Nature Scot<br />\
+    <img src="styles/legend/NaturesReserves_2_1.png" /> RSPB<br />\
+    <img src="styles/legend/NaturesReserves_2_2.png" /> Scottish Wildlife Trust<br />\
+    <img src="styles/legend/NaturesReserves_2_3.png" /> Trees for Life<br />\
+    <img src="styles/legend/NaturesReserves_2_4.png" /> Other<br />' });
 
-lyr_BingVirtualEarth_0.setVisible(true);lyr_OSM_railways_1.setVisible(true);
-var layersList = [lyr_BingVirtualEarth_0,lyr_OSM_railways_1];
-lyr_OSM_railways_1.set('fieldAliases', {'full_id': 'full_id', 'osm_id': 'osm_id', 'osm_type': 'osm_type', 'railway': 'railway', 'source_geo': 'source_geo', 'covered': 'covered', 'surface': 'surface', 'highway': 'highway', 'foot': 'foot', 'bicycle': 'bicycle', 'descriptio': 'descriptio', 'railway_tr': 'railway_tr', 'disused_ra': 'disused_ra', 'tunnel': 'tunnel', 'source_bri': 'source_bri', 'bridge_ref': 'bridge_ref', 'service': 'service', 'old_ref': 'old_ref', 'highspeed': 'highspeed', 'source_ele': 'source_ele', 'voltage': 'voltage', 'passenger_': 'passenger_', 'frequency': 'frequency', 'razed_rail': 'razed_rail', 'alt_name': 'alt_name', 'bridge': 'bridge', 'abandoned_': 'abandoned_', 'ref_seq': 'ref_seq', 'ref_elr': 'ref_elr', 'railway__1': 'railway__1', 'layer': 'layer', 'cutting': 'cutting', 'usage': 'usage', 'tracks': 'tracks', 'ref_lor': 'ref_lor', 'ref': 'ref', 'railway_tp': 'railway_tp', 'railway_aw': 'railway_aw', 'passenger': 'passenger', 'operator_w': 'operator_w', 'operator': 'operator', 'name': 'name', 'maxspeed': 'maxspeed', 'loading_ga': 'loading_ga', 'gauge': 'gauge', 'embankment': 'embankment', 'electrifie': 'electrifie', });
-lyr_OSM_railways_1.set('fieldImages', {'full_id': '', 'osm_id': '', 'osm_type': '', 'railway': '', 'source_geo': '', 'covered': '', 'surface': '', 'highway': '', 'foot': '', 'bicycle': '', 'descriptio': '', 'railway_tr': '', 'disused_ra': '', 'tunnel': '', 'source_bri': '', 'bridge_ref': '', 'service': '', 'old_ref': '', 'highspeed': '', 'source_ele': '', 'voltage': '', 'passenger_': '', 'frequency': '', 'razed_rail': '', 'alt_name': '', 'bridge': '', 'abandoned_': '', 'ref_seq': '', 'ref_elr': '', 'railway__1': '', 'layer': '', 'cutting': '', 'usage': '', 'tracks': '', 'ref_lor': '', 'ref': '', 'railway_tp': '', 'railway_aw': '', 'passenger': '', 'operator_w': '', 'operator': '', 'name': '', 'maxspeed': '', 'loading_ga': '', 'gauge': '', 'embankment': '', 'electrifie': '', });
-lyr_OSM_railways_1.set('fieldLabels', {'full_id': 'inline label - always visible', 'osm_id': 'no label', 'osm_type': 'no label', 'railway': 'inline label - visible with data', 'source_geo': 'no label', 'covered': 'no label', 'surface': 'no label', 'highway': 'no label', 'foot': 'no label', 'bicycle': 'no label', 'descriptio': 'no label', 'railway_tr': 'no label', 'disused_ra': 'no label', 'tunnel': 'no label', 'source_bri': 'no label', 'bridge_ref': 'no label', 'service': 'no label', 'old_ref': 'no label', 'highspeed': 'no label', 'source_ele': 'no label', 'voltage': 'no label', 'passenger_': 'no label', 'frequency': 'no label', 'razed_rail': 'no label', 'alt_name': 'no label', 'bridge': 'no label', 'abandoned_': 'no label', 'ref_seq': 'no label', 'ref_elr': 'no label', 'railway__1': 'no label', 'layer': 'no label', 'cutting': 'no label', 'usage': 'no label', 'tracks': 'no label', 'ref_lor': 'no label', 'ref': 'no label', 'railway_tp': 'no label', 'railway_aw': 'no label', 'passenger': 'no label', 'operator_w': 'no label', 'operator': 'no label', 'name': 'no label', 'maxspeed': 'no label', 'loading_ga': 'no label', 'gauge': 'no label', 'embankment': 'no label', 'electrifie': 'no label', });
-lyr_OSM_railways_1.on('precompose', function(evt) {
+lyr_OSMStandard_0.setVisible(true);lyr_GoogleSatelliteHybrid_1.setVisible(true);lyr_NaturesReserves_2.setVisible(true);
+var layersList = [lyr_OSMStandard_0,lyr_GoogleSatelliteHybrid_1,lyr_NaturesReserves_2];
+lyr_NaturesReserves_2.set('fieldAliases', {'id': 'id', 'Name': 'Name', 'VisitorCen': 'VisitorCen', 'Size (hec)': 'Size (hec)', 'Notes': 'Notes', 'Cafe': 'Cafe', 'Type': 'Type', 'Habitats': 'Habitats', 'Website': 'Website', 'Managed by': 'Managed by', 'Toilets': 'Toilets', });
+lyr_NaturesReserves_2.set('fieldImages', {'id': 'Hidden', 'Name': 'TextEdit', 'VisitorCen': 'TextEdit', 'Size (hec)': 'TextEdit', 'Notes': 'TextEdit', 'Cafe': 'TextEdit', 'Type': 'TextEdit', 'Habitats': 'TextEdit', 'Website': 'TextEdit', 'Managed by': 'TextEdit', 'Toilets': 'TextEdit', });
+lyr_NaturesReserves_2.set('fieldLabels', {'Name': 'inline label - visible with data', 'VisitorCen': 'inline label - visible with data', 'Size (hec)': 'inline label - visible with data', 'Notes': 'inline label - visible with data', 'Cafe': 'inline label - visible with data', 'Type': 'inline label - visible with data', 'Habitats': 'inline label - visible with data', 'Website': 'inline label - visible with data', 'Managed by': 'inline label - visible with data', 'Toilets': 'inline label - visible with data', });
+lyr_NaturesReserves_2.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
---
+++ styles/NaturesReserves_2_style.js
@@ -0,0 +1,113 @@
+var size = 0;
+var placement = 'point';
+function categories_NaturesReserves_2(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case 'Nature Scot':
+                    return [ new ol.style.Style({
+        image: new ol.style.Icon({
+                  imgSize: [580, 580],
+                  scale: 0.04310344827586207,
+                  anchor: [290.0, 290.0],
+                  anchorXUnits: "pixels",
+                  anchorYUnits: "pixels",
+                  rotation: 0.0,
+                  src: "styles/landuse_deciduous.svg"
+            }),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'RSPB':
+                    return [ new ol.style.Style({
+        image: new ol.style.Icon({
+                  imgSize: [580, 580],
+                  scale: 0.04310344827586207,
+                  anchor: [290.0, 290.0],
+                  anchorXUnits: "pixels",
+                  anchorYUnits: "pixels",
+                  rotation: 0.0,
+                  src: "styles/landuse_deciduous_1.svg"
+            }),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Scottish Wildlife Trust':
+                    return [ new ol.style.Style({
+        image: new ol.style.Icon({
+                  imgSize: [580, 580],
+                  scale: 0.04310344827586207,
+                  anchor: [290.0, 290.0],
+                  anchorXUnits: "pixels",
+                  anchorYUnits: "pixels",
+                  rotation: 0.0,
+                  src: "styles/landuse_deciduous_2.svg"
+            }),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Trees for Life':
+                    return [ new ol.style.Style({
+        image: new ol.style.Icon({
+                  imgSize: [580, 580],
+                  scale: 0.04310344827586207,
+                  anchor: [290.0, 290.0],
+                  anchorXUnits: "pixels",
+                  anchorYUnits: "pixels",
+                  rotation: 0.0,
+                  src: "styles/landuse_deciduous_3.svg"
+            }),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+default:
+                    return [ new ol.style.Style({
+        image: new ol.style.Icon({
+                  imgSize: [580, 580],
+                  scale: 0.04310344827586207,
+                  anchor: [290.0, 290.0],
+                  anchorXUnits: "pixels",
+                  anchorYUnits: "pixels",
+                  rotation: 0.0,
+                  src: "styles/landuse_deciduous_4.svg"
+            }),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_NaturesReserves_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("Managed by");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+    var style = categories_NaturesReserves_2(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
